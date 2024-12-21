792afdb87c5f7ec1c2a77d577355183135633aaa
iambasantarai <iambasantarai@gmail.com>
2024-12-21 17:41:08 +0545

    Create a simple http server to serve static files

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.Handle("/", http.FileServer(http.Dir("./ui")))
+
+	fmt.Printf("Application started.\nLink: http://localhost:%d\n", 8000)
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatalf("ERROR: %s", err.Error())
+	}
 }
