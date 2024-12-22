e0432e267d37c9d544f611125a91bc5f039aecea
iambasantarai <iambasantarai@gmail.com>
2024-12-22 21:27:52 +0545

    Show CPU and disk information

--- main.go
+++ main.go
@@ -7,10 +7,23 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/mem"
 )
 
 type MemoryInfo struct {
+	Bytes uint64 `json:"bytes"`
+	MiB   uint64 `json:"MiB"`
+	GiB   uint64 `json:"GiB"`
+}
+
+type CPUInfo struct {
+	ModelName string    `json:"modelName"`
+	Cores     []float64 `json:"cores"`
+}
+
+type DiskInfo struct {
 	Bytes uint64 `json:"bytes"`
 	MiB   uint64 `json:"MiB"`
 	GiB   uint64 `json:"GiB"`
@@ -22,8 +35,16 @@ type MemoryData struct {
 	Availability MemoryInfo `json:"availability"`
 }
 
+type DiskData struct {
+	Capacity     DiskInfo `json:"capacity"`
+	Usage        DiskInfo `json:"usage"`
+	Availability DiskInfo `json:"availability"`
+}
+
 type SysInfo struct {
 	Memory MemoryData `json:"memory"`
+	CPU    CPUInfo    `json:"cpu"`
+	Disk   DiskData   `json:"disk"`
 }
 
 func main() {
@@ -41,8 +62,8 @@ func sysInfoHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 
-	memT := time.NewTicker(time.Second)
-	defer memT.Stop()
+	tickerT := time.NewTicker(time.Second)
+	defer tickerT.Stop()
 
 	clientDisconnected := r.Context().Done()
 
@@ -51,36 +72,75 @@ func sysInfoHandler(w http.ResponseWriter, r *http.Request) {
 		case <-clientDisconnected:
 			fmt.Println("Client has been disconnected.")
 			return
-		case <-memT.C:
-			m, err := mem.VirtualMemory()
+		case <-tickerT.C:
+			memStat, err := mem.VirtualMemory()
 			if err != nil {
 				log.Printf("Unable to get memory stats: %s", err.Error())
+				return
+			}
+
+			cpuStat, err := cpu.Info()
+			if err != nil {
+				log.Printf("Unable to get cpu stats: %s", err.Error())
+				return
+			}
+
+			usedCPUPercentage, err := cpu.Percent(0, true)
+			if err != nil {
+				log.Printf("Unable to get used cpu stats: %s", err.Error())
+				return
+			}
+
+			diskStat, err := disk.Usage("/")
+			if err != nil {
+				log.Printf("Unable to get disk stats: %s", err.Error())
 				return
 			}
 
 			data := SysInfo{
 				Memory: MemoryData{
 					Capacity: MemoryInfo{
-						Bytes: m.Total,
-						MiB:   m.Total / (1024 * 1024),
-						GiB:   m.Total / (1024 * 1024 * 1024),
+						Bytes: memStat.Total,
+						MiB:   memStat.Total / (1024 * 1024),
+						GiB:   memStat.Total / (1024 * 1024 * 1024),
 					},
 					Usage: MemoryInfo{
-						Bytes: m.Used,
-						MiB:   m.Used / (1024 * 1024),
-						GiB:   m.Used / (1024 * 1024 * 1024),
+						Bytes: memStat.Used,
+						MiB:   memStat.Used / (1024 * 1024),
+						GiB:   memStat.Used / (1024 * 1024 * 1024),
 					},
 					Availability: MemoryInfo{
-						Bytes: m.Free,
-						MiB:   m.Free / (1024 * 1024),
-						GiB:   m.Free / (1024 * 1024 * 1024),
+						Bytes: memStat.Free,
+						MiB:   memStat.Free / (1024 * 1024),
+						GiB:   memStat.Free / (1024 * 1024 * 1024),
+					},
+				},
+				CPU: CPUInfo{
+					ModelName: cpuStat[0].ModelName,
+					Cores:     usedCPUPercentage,
+				},
+				Disk: DiskData{
+					Capacity: DiskInfo{
+						Bytes: diskStat.Total,
+						MiB:   diskStat.Total / (1024 * 1024),
+						GiB:   diskStat.Total / (1024 * 1024 * 1024),
+					},
+					Usage: DiskInfo{
+						Bytes: diskStat.Used,
+						MiB:   diskStat.Used / (1024 * 1024),
+						GiB:   diskStat.Used / (1024 * 1024 * 1024),
+					},
+					Availability: DiskInfo{
+						Bytes: diskStat.Free,
+						MiB:   diskStat.Free / (1024 * 1024),
+						GiB:   diskStat.Free / (1024 * 1024 * 1024),
 					},
 				},
 			}
 
 			jsonData, err := json.Marshal(data)
 			if err != nil {
-				log.Printf("Unable to marshal memory data: %s", err.Error())
+				log.Printf("Unable to marshal data: %s", err.Error())
 				return
 			}
 
